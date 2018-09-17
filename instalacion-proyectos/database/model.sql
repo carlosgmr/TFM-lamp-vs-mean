95b72e4287b4df46177e1f9e1e0816fe4a63729a
Carlos Molina <cmolinaronceros@gmail.com>
2018-09-17 20:48:40 +0200

    Actualizando instrucciones instalación proyectos

--- instalacion-proyectos/database/model.sql
+++ instalacion-proyectos/database/model.sql
@@ -282,11 +282,6 @@ CREATE TABLE IF NOT EXISTS `registry` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
 -- -----------------------------------------------------
 -- Data for table `administrator`
 -- -----------------------------------------------------
